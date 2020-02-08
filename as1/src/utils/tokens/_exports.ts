6d057a8b4058bcffb55cecd16e1081aa1f2f547c
Marek Sautter <marek.sautter@gmail.com>
2020-02-07 16:18:53 -0800

    Removed functions that were preventing deployment to repl

--- as1/src/utils/tokens/_exports.ts
+++ as1/src/utils/tokens/_exports.ts
@@ -3,14 +3,14 @@ export { isOperator, hasOperator } from './operators';
 export { isSeparator, hasSeparator } from './separators';
 export { isValidIdentifier } from './identifiers';
 export { isRealnum } from './realnums';
-export { isKeychar, hasKeychar, findKeychars } from './keychars';
+export { isKeychar, hasKeychar } from './keychars';
 
 import { isKeyword, hasKeyword } from './keywords';
 import { isOperator, hasOperator } from './operators';
 import { isSeparator, hasSeparator } from './separators';
 import { isValidIdentifier } from './identifiers';
 import { isRealnum } from './realnums';
-import { isKeychar, hasKeychar, findKeychars } from './keychars';
+import { isKeychar, hasKeychar } from './keychars';
 
 export const Tokens = {
     isKeyword,
@@ -22,6 +22,5 @@ export const Tokens = {
     isValidIdentifier,
     isRealnum,
     isKeychar,
-    hasKeychar,
-    findKeychars
+    hasKeychar
 };
--- as1/src/utils/tokens/keychars.ts
+++ as1/src/utils/tokens/keychars.ts
@@ -1,16 +1,16 @@
 import { separators } from './separators';
 import { operators } from './operators';
 
-const keychars = [
-    ...separators.map((separator) => ({
-        value: separator,
-        type: 'separator'
-    })),
-    ...operators.map((operator) => ({
-        value: operator,
-        type: 'operator'
-    }))
-];
+// const keychars = [
+//     ...separators.map((separator) => ({
+//         value: separator,
+//         type: 'separator'
+//     })),
+//     ...operators.map((operator) => ({
+//         value: operator,
+//         type: 'operator'
+//     }))
+// ];
 
 const keycharsArr = [...separators, ...operators];
 
@@ -18,22 +18,22 @@ export const isKeychar = (char: string) => keycharsArr.includes(char);
 
 export const hasKeychar = (word: string) => keycharsArr.some((char) => word.includes(char));
 
-export const findKeychars = (word: string) => {
-    let isFound = false;
-    let foundKeychars: any[] = [];
-    word.split('').forEach((char, index) => {
-        if (isKeychar(char)) {
-            isFound = true;
-            let foundKeychar = keychars.find((keychar) => keychar.value === char);
-            foundKeychars.push({
-                value: foundKeychar?.value,
-                type: foundKeychar?.type,
-                index: index
-            });
-        }
-    });
-    return {
-        isFound,
-        foundKeychars
-    };
-};
+// export const findKeychars = (word: string) => {
+//     let isFound = false;
+//     let foundKeychars: any[] = [];
+//     word.split('').forEach((char, index) => {
+//         if (isKeychar(char)) {
+//             isFound = true;
+//             let foundKeychar = keychars.find((keychar) => keychar.value === char);
+//             foundKeychars.push({
+//                 value: foundKeychar?.value,
+//                 type: foundKeychar?.type,
+//                 index: index
+//             });
+//         }
+//     });
+//     return {
+//         isFound,
+//         foundKeychars
+//     };
+// };
