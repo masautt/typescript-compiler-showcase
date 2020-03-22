a622f8cbf0fb5884d1189cc6193fb3e5a1549aaf
masautt <marek.sautter@gmail.com>
2020-03-21 23:21:01 -0700

    Condensed Lexer into single file to prepare for parser

--- as2/lib/index.js
+++ as2/lib/index.js
@@ -1,2 +1,4 @@
 "use strict";
-console.log("hello world");
+Object.defineProperty(exports, "__esModule", { value: true });
+var lexer_1 = require("./lexer");
+lexer_1.lexer("int num1, num2, large$");
--- as2/src/index.ts
+++ as2/src/index.ts
@@ -1,1 +1,3 @@
-console.log("hello world");
+import { lexer } from "./lexer";
+
+lexer("int num1, num2, large$");
